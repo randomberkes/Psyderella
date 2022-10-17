4370b8ef171ebd098b10bce9f91bc3791c53bc3e
randomberkes <randomberkes@gmail.com>
2022-10-17 13:05:53 +0200

    Like button created and added to project

--- psyderella-frontend/src/App.js
+++ psyderella-frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Header from "./components/Header"
 import Dislike from "./components/actions/Dislike"
+import Like from "./components/actions/Like"
 import './App.css';
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
         <div className="app">
             <Header/>
             <Dislike/>
+            <Like/>
         </div>
     )
 }
---
+++ psyderella-frontend/src/components/actions/Like.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Like = ({ userId, modifySuperficialChoices }) => (
+  <button
+    type="button"
+    onClick={() => {}}
+  >
+    <img src="images/misc/like.png" alt="Like User" />
+  </button>
+);
+
+export default Like;
