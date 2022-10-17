190830c06bcf099865fc3a7401d8eabe1495cbd7
randomberkes <randomberkes@gmail.com>
2022-10-17 13:01:24 +0200

    Dislike button created and added to project

--- psyderella-frontend/src/App.js
+++ psyderella-frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Header from "./components/Header"
+import Dislike from "./components/actions/Dislike"
 import './App.css';
 
 const App = () => {
     return (
         <div className="app">
             <Header/>
+            <Dislike/>
         </div>
     )
 }
---
+++ psyderella-frontend/src/components/actions/Dislike.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dislike = ({ userId, modifySuperficialChoices }) => (
+  <button
+    type="button"
+    onClick={() => {}}
+  >
+    <img src="images/misc/dislike.png" alt="Dislike User" />
+  </button>
+);
+
+export default Dislike;
