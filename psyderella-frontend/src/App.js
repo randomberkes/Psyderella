82c6d35e1394d16b3517d36e4102b7eb262797ee
randomberkes <randomberkes@gmail.com>
2022-10-17 12:34:55 +0200

    Header component created and added to project

--- psyderella-frontend/src/App.js
+++ psyderella-frontend/src/App.js
@@ -1,8 +1,8 @@
 import React from "react"
-import Logo from "./components/Logo"
+import Header from "./components/Header"
 
 const App = () => {
-    return <Logo/>
+    return <Header/>
 }
 
 export default App;
