0a046de9fa0461e339c470a99daac92831cd1874
Berkes-Scherer David <randomberkes@gmail.com>
2022-11-12 13:37:44 +0100

    Pages Dashboard and Home added to project

--- psyderella-backend/index.js
+++ psyderella-backend/index.js
@@ -2,6 +2,12 @@ const PORT = 8080
 
 const express = require('express')
 
+const { MongoClient } = require('mongodb')
+
 const app = express()
+
+app.get('/', (req, res) => {
+    res.json("Hello")
+})
 
 app.listen(PORT, () => console.log(' Server running on PORT ' + PORT))
