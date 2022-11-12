86dccce5c539a65b9b501a5e7018f20c6fe7243c
Berkes-Scherer David <randomberkes@gmail.com>
2022-11-12 17:06:54 +0100

    Form created in AuthModal component and css added

--- psyderella-frontend/src/components/AuthModal.js
+++ psyderella-frontend/src/components/AuthModal.js
@@ -1,6 +1,59 @@
-const AuthModal = () => {
+import { useState } from 'react'
+
+const AuthModal = ({ setShowModal }) => {
+
+    const [ eamil, setEmail ] = useState(null)
+    const [ password, setPassword ] = useState(null)
+    const [ confirmPassword, setConfirmPassword ] = useState(null)
+    const [ error, setError ] = useState(null)
+
+    const handleClick = () => {
+        setShowModal(false)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+    }
+
+    const isSignUp = true;
+
     return (
-        <div>AuthModal</div>
+        <div className="auth-modal">
+            <div className="close-icon" onClick={handleClick}>x</div>
+            <h2>{isSignUp ? "CREATE ACCOUNT" : "LOG IN"}</h2>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    palcejolder="email"
+                    required={true}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    palcejolder="password"
+                    required={true}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <input
+                    type="confirmPassword"
+                    id="confirmPassword"
+                    name="confirmPassword"
+                    palcejolder="confirmPassword"
+                    required={true}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+                <input className="secondary-button" type="submit">
+
+                </input>
+                <p>{error}</p>
+
+            </form>
+        </div>
     )
 }
 
--- psyderella-frontend/src/pages/Home.js
+++ psyderella-frontend/src/pages/Home.js
@@ -1,25 +1,30 @@
 import Navbar from "../components/Navbar"
-import Nav from "../components/Navbar"
+import { useState } from 'react'
+import AuthModal from "../components/AuthModal"
 
 const Home = () => {
+
+    const [showModal, setShowModal] = useState(false)
 
     const authToken = true
 
     const handleClick = () => {
         console.log("clicked")
+        setShowModal(true)
     }
 
     return (
         <>
-            
-           
             <div className="overlay">
-                <Navbar minimal={false} authToken={false}/>
+                <Navbar minimal={false} authToken={false} setShowModal={setShowModal} showModal={showModal} />
                 <div className="Home">
                     <h1>Swipe Right</h1>
                     <button className="primary-button" onClick={handleClick}>
                         {authToken ? 'Signout' : 'Create Account'}
                     </button>
+                    {showModal && (
+                        <AuthModal setShowModal={setShowModal} />
+                    )}
                 </div>
             </div>
         </>
